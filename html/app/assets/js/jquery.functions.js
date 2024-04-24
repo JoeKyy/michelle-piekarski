eb123c24996e9a0f461911af96748aa59231c5b3
Jhomar 'Joe' Nando <contato@joekyy.com.br>
2024-04-23 22:05:21 -0300

    feat: finishing header and adding footer

--- html/app/assets/js/jquery.functions.js
+++ html/app/assets/js/jquery.functions.js
@@ -1,3 +1,21 @@
 $(function () {
+    $('.nav-button').click(function(){
+		if (!$(this).hasClass('is-active')) {
+			$(this).addClass('is-active');
+			$('.menu nav').addClass('nav-menu--open');
+			$('.menu nav').removeClass('nav-menu--close');
+		} else {
+			$(this).removeClass('is-active');
+			$('.menu nav').removeClass('nav-menu--open');
+			$('.menu nav').addClass('nav-menu--close');
+		}
+	});
 
+	$('header .menu nav ul li a').click(function(){
+		if ($('.nav-button').hasClass('is-active')) {
+			$('.nav-button').removeClass('is-active');
+			$('.menu nav').removeClass('nav-menu--open');
+			$('.menu nav').addClass('nav-menu--close');
+		}
+	});
 });
